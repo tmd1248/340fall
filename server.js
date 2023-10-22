dcf145263376454d16761aa41aa8f5cfc93333f8
Tyler Day <tmd1248@Wgmail.com>
2023-10-21 21:08:05 -0700

    fixed an issue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
-
+const inventoryRoute = require("./controllers/invController")
 
 /* ***********************
  * View engine and Templates
